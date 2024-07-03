f208c71bb964c3b7aebd9868f3f0a3b7ca4cdcaf
DonavanGrobler <donavangrobler2@gmail.com>
2024-07-03 21:14:05 +0200

    Adds custom hook for authentication

--- src/App.jsx
+++ src/App.jsx
@@ -12,61 +12,10 @@ import UpdatePlace from "./places/pages/UpdatePlace";
 import Auth from "./user/pages/Auth";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import { AuthContext } from "./shared/context/auth-context";
-
-let logoutTimer;
+import { useAuth } from "./shared/hooks/auth-hook";
 
 const App = () => {
-  const [token, setToken] = useState(false);
-  const [userId, setUserId] = useState(false);
-  const [tokenExperationDate, setTokenExperationDate] = useState();
-
-  const login = useCallback((uid, token, experationDate) => {
-    setToken(token);
-    setUserId(uid);
-    const tokenExperationDate =
-      experationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
-    setTokenExperationDate(tokenExperationDate);
-    localStorage.setItem(
-      "userData",
-      JSON.stringify({
-        userId: uid,
-        token: token,
-        experation: tokenExperationDate.toISOString(),
-      })
-    );
-  }, []);
-
-  const logout = useCallback(() => {
-    setToken(null);
-    setTokenExperationDate(null);
-    setUserId(null);
-    localStorage.removeItem("userData");
-  }, []);
-
-  useEffect(() => {
-    if (token && tokenExperationDate) {
-      const remainingTime =
-        tokenExperationDate.getTime() - new Date().getTime();
-      logoutTimer = setTimeout(logout, remainingTime);
-    } else {
-      clearTimeout(logoutTimer);
-    }
-  }, [token, logout, tokenExperationDate]);
-
-  useEffect(() => {
-    const storedData = JSON.parse(localStorage.getItem("userData"));
-    if (
-      storedData &&
-      storedData.token &&
-      new Date(storedData.experation) > new Date()
-    ) {
-      login(
-        storedData.userId,
-        storedData.token,
-        new Date(storedData.experation)
-      );
-    }
-  }, [login]);
+  const { token, login, logout, userId } = useAuth();
 
   let routes;
 
